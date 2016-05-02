b7dcc1858f0c1ad199c83b13f224831b7e1fe1a0
eduarmreyes <eduar.mreyes@gmail.com>
2016-05-01 22:25:56 -0600

    Add Twitter section and footer

---
+++ js/twitter-config.js
@@ -0,0 +1,11 @@
+var config1 = {
+  "id": '726621360579993601',
+  "domId": 'tweets',
+  "maxTweets": 3,
+  "enableLinks": false,
+  "showPermalinks": false,
+  "showUser": false,
+  "showTime": true
+};
+
+twitterFetcher.fetch(config1);
